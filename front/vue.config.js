1ac14c41c4abc454c699cfdd340526e8793b4f9b
sangwoo <tkddn3367@gmail.com>
2023-01-08 09:46:19 +0900

    refactor indent

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: "../src/main/resources/static",
-  indexPath: "../static/index.html",
-  devServer: {
-    port: 3000
-  },
+    transpileDependencies: true,
+    outputDir: "../src/main/resources/static",
+    indexPath: "../static/index.html",
+    devServer: {
+        port: 3000
+    },
 })
